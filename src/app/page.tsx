7b8a5698a6a7d3761f1b9bf565100f0b02a2d87d
Jen <evunti@gmail.com>
2025-03-12 11:37:18 -0400

    made the add item and move item functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,19 @@
-export default function ToDoList() {
+"use client";
+import { useState } from "react";
+
+export default function CreateListItem() {
+  const [wantToDoItem, setWantToDoItem] = useState<string[]>([]);
+  const [needToDoItem, setNeedToDoItem] = useState<string[]>([]);
+
+
+  const addWantToDoItem = () => {
+    setWantToDoItem([...wantToDoItem, ""]);
+  };
+  const moveToNeedToDo = (index:number) => {
+    const itemToMove = wantToDoItem[index];
+    setWantToDoItem(wantToDoItem.filter((_, i) => i !== index));
+    setNeedToDoItem([...needToDoItem, itemToMove]);
+  }
   return (
     <div>
       <main>
@@ -8,31 +23,39 @@ export default function ToDoList() {
         <div className="ListTablesContainer">
           <table className="LeftTable">
             <tr>
-              <th>Want To Do</th>
+              <th>
+                <button className="cursor-pointer" onClick={addWantToDoItem}>
+                  Want To Do
+                </button>
+              </th>
             </tr>
-            <tr>
-              <td>Exmaple</td>
-            </tr>
-            <tr>
-              <td>Exmaple</td>
-            </tr>
-            <tr>
-              <td>Exmaple</td>
-            </tr>
+            <tbody>
+              {wantToDoItem.map((item, index) => (
+                <tr key={index}>
+                  <td>
+                    <input
+                      value={item}
+                      onChange={(e) => {
+                        const newItems = [...wantToDoItem];
+                        newItems[index] = e.target.value;
+                        setWantToDoItem(newItems);
+                      }}
+                    />
+                    <button className="ml-2" onClick={()=> moveToNeedToDo(index)}>></button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
           <table className="RightTable">
             <tr>
               <th>Need To Do</th>
-            </tr>
-            <tr>
-              <td>Exmaple</td>
-            </tr>
-            <tr>
-              <td>Exmaple</td>
-            </tr>
-            <tr>
-              <td>Exmaple</td>
-            </tr>
+              </tr>
+            <tbody>
+              {needToDoItem.map((item, index)=>(
+                <tr key={index}><td>{item}</td></tr>
+              ))}
+              </tbody>
           </table>
         </div>
       </main>
