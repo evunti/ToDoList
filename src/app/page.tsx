636fadb84b24f4acc0630ef3fc3e5a54bc7d26ad
Jen <evunti@gmail.com>
2025-03-12 10:16:03 -0400

    added some tables and css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,41 @@
 export default function ToDoList() {
   return (
     <div>
-      <table>
-        <tr>
-          <th>Want To Do</th>
-        </tr>
-        <tr>
-          <td>Exmaple</td>
-        </tr>
-        <tr>
-          <td>Exmaple</td>
-        </tr>
-        <tr>
-          <td>Exmaple</td>
-        </tr>
-      </table>
-      <table>
-        <tr>
-          <th>Need To Do</th>
-        </tr>
-        <tr>
-          <td>Exmaple</td>
-        </tr>
-        <tr>
-          <td>Exmaple</td>
-        </tr>
-        <tr>
-          <td>Exmaple</td>
-        </tr>
-      </table>
+      <main>
+        <h1 className="text-2xl font-bold flex justify-center mt-20">
+          Ideas List
+        </h1>
+        <div className="ListTablesContainer">
+          <table className="LeftTable">
+            <tr>
+              <th>Want To Do</th>
+            </tr>
+            <tr>
+              <td>Exmaple</td>
+            </tr>
+            <tr>
+              <td>Exmaple</td>
+            </tr>
+            <tr>
+              <td>Exmaple</td>
+            </tr>
+          </table>
+          <table className="RightTable">
+            <tr>
+              <th>Need To Do</th>
+            </tr>
+            <tr>
+              <td>Exmaple</td>
+            </tr>
+            <tr>
+              <td>Exmaple</td>
+            </tr>
+            <tr>
+              <td>Exmaple</td>
+            </tr>
+          </table>
+        </div>
+      </main>
     </div>
   );
 }
